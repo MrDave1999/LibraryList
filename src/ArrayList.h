f20e0ab13ca6f5ad91bfd5b8f4c83bb144931aed
David Román <43916038+MrDave1999@users.noreply.github.com>
2020-01-07 15:25:15 -0500

    Header File
    Se agregó algunos casting para tener compatibilidad con C++.

--- src/ArrayList.h
+++ src/ArrayList.h
@@ -1,4 +1,4 @@
- #ifndef _ARRAYLIST_H
+#ifndef _ARRAYLIST_H
 #define _ARRAYLIST_H
 
 #include "MemoryManagement.h"
@@ -13,11 +13,11 @@ struct ArrayList
 
 typedef struct ArrayList ArrayList;
 
-#define newArrayList() __new__(ARRAYLIST, sizeof(ArrayList))
+#define newArrayList() (ArrayList*)__new__(ARRAYLIST, sizeof(ArrayList))
 
 #define forAL(type, nameVar, _ptr) \
 	_ptr->i = 0; \
-	for(type* nameVar; _ptr->i != _ptr->count && (nameVar = _ptr->ptr[_ptr->i]) != NULL; ++_ptr->i)
+	for(type* nameVar; _ptr->i != _ptr->count && (nameVar = (type*)_ptr->ptr[_ptr->i]) != NULL; ++_ptr->i)
 
 #ifdef __cplusplus
 extern "C" {
@@ -25,7 +25,7 @@ extern "C" {
 
 void* getAL(ArrayList* al, size_t index);
 void* setAL(ArrayList* al, size_t index, void* newObject);
-void clearAL(ArrayList* al);
+void clearAL(void* al);
 uint8_t setCapacity(ArrayList* al, const size_t newCapacity);
 uint8_t addAL(ArrayList* al, void* refObject);
 uint8_t removeAL(ArrayList* al, int(*funcComparator)(void*), const uint8_t option);
