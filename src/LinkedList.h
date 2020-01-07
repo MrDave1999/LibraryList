456d6dedb854ec5552120965e6445e2918750d21
David Román <43916038+MrDave1999@users.noreply.github.com>
2020-01-07 15:29:07 -0500

    Header File

--- src/LinkedList.h
+++ src/LinkedList.h
@@ -20,18 +20,18 @@ struct LinkedList
 typedef struct LinkedList LinkedList;
 typedef struct Node Node;
 
-#define newLinkedList() __new__(LINKEDLIST, sizeof(LinkedList))
+#define newLinkedList() (LinkedList*)__new__(LINKEDLIST, sizeof(LinkedList))
 
 #define forLK(type, nameVar, ptr) \
 	ptr->aux = ptr->pBegin; \
-	for(type* nameVar; ptr->aux != NULL && (nameVar = ptr->aux->object) != NULL; ptr->aux = ptr->aux->sig)
+	for(type* nameVar; ptr->aux != NULL && (nameVar = (type*)ptr->aux->object) != NULL; ptr->aux = ptr->aux->sig)
 
 #ifdef __cplusplus
 extern "C" {
 #endif /* __cplusplus */
 
 uint8_t addLK(LinkedList* lk, void* newObject);
-void clearLK(LinkedList* lk);
+void clearLK(void* lk);
 void* getLK(LinkedList* lk, size_t index);
 void* setLK(LinkedList* lk, size_t index, void* newObject);
 uint8_t removeLK(LinkedList* lk, int(*funcComparator)(void*), const uint8_t option);
