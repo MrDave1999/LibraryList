8822475b82006735981421072f7ab773a8e0d541
David Román <43916038+MrDave1999@users.noreply.github.com>
2020-05-30 15:37:05 -0500

    Update test2.c

--- Test/test2.c
+++ Test/test2.c
@@ -1,16 +1,21 @@
 #include <stdio.h>
-#include <stdlib.h> 
+#include <stdlib.h>
+#include <string.h> 
 #include <lst/List.h>
+
+boolean nameEquals(const void* p, const void* key)
+{
+	return strcmp(p, key) == 0;
+}
 
 int main()
 {
 	Stack* listNames = newStack();
-	
 	push(listNames, "David");
 	push(listNames, "Roberto");
 	push(listNames, "Joel");
 	push(listNames, "Josias");
-	printf("(1) isEmpty: %d, size: %d\n\n", isEmpty(listNames), size(listNames));
+	printf("(1) isEmpty: %d, size: %d, Top element: %s\n\n", isEmpty(listNames), size(listNames), (char*)getTop(listNames));
 	foreach(char, name, listNames)
 		printf("%s\n", name);
 		
@@ -31,6 +36,9 @@ int main()
 	printf("(3) isEmpty: %d, size: %d\n\n", isEmpty(listNames), size(listNames));
 	push(listNames, "Lorenzo");
 	push(listNames, "Victor");
+	push(listNames, "Pepeito");
+	
+	printf("\nElement search: %s\n", (char*)find(listNames, "Victor", nameEquals));
 	
 	clear(listNames);
 	
@@ -38,10 +46,6 @@ int main()
 		printf("%s\n", name);
 		
 	delete(listNames);
-	
-	printf("Stacks: %d\n", registers[STACK].count);
-	
 	getchar();
     return 0;
 }
-
