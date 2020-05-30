a83c402d2c12d70301d9edc487ce3e492a6de200
David Román <43916038+MrDave1999@users.noreply.github.com>
2020-05-30 15:36:22 -0500

    Update test1.c

--- Test/test1.c
+++ Test/test1.c
@@ -2,15 +2,20 @@
 #include <stdlib.h>
 #include <lst/List.h>
 
+boolean intEquals(const void* p, const void* key)
+{
+	return *(int*)p == *(int*)key;
+}
+
 int main()
 {
 	Queue* listNumber = newQueue();
-	
+	int key;
 	enqueue(listNumber, 12);
 	enqueue(listNumber, 4);
 	enqueue(listNumber, 1);
 	enqueue(listNumber, -23);
-	printf("(1) isEmpty: %d, size: %d\n\n", isEmpty(listNumber), size(listNumber));
+	printf("(1) isEmpty: %d, size: %d, Front element: %d\n\n", isEmpty(listNumber), size(listNumber), *(int*)getFront(listNumber));
 	foreach(int, number, listNumber)
 		printf("%d\n", *number);
 		
@@ -31,6 +36,9 @@ int main()
 	printf("(3) isEmpty: %d, size: %d\n\n", isEmpty(listNumber), size(listNumber));
 	enqueue(listNumber, 2000);
 	enqueue(listNumber, 232);
+	enqueue(listNumber, 500);
+	key = 232;
+	printf("\nElement search: %d\n", *(int*)find(listNumber, &key, intEquals));
 	
 	clear(listNumber);
 	
